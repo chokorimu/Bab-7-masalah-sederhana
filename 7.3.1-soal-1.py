a3a6b6813fcb15d26ce11f16f877c06f5dab2274
Chokorimu <rezondavar@gmail.com>
2025-10-19 17:06:33 +0700

    Add solution for Soal 1

---
+++ 7.3.1-soal-1.py
@@ -0,0 +1,24 @@
+print("programm mencari median perusahaan")
+#input() yang akan membaca baris pertama
+N=int(input())
+#buat lis kosong untuk menampung semua nilai
+daftar_nilai=[]
+#loop sebanyak N kali untuk membaca nilai
+for _ in range(N) :
+    #Baca nilai di tiap baris dan ubah jadi angka
+    nilai=int(input())
+    #Masukan angka dalam list
+    daftar_nilai.append(nilai)
+    #urutkan daftar nilai dari yang terkecil sampai yang terbesar
+    daftar_nilai.sort()
+if N %2==1 :
+    #kasus ganjil
+    index_tengah = N//2
+    median = daftar_nilai[index_tengah]
+else :
+    #kasus genap
+    index_kiri = N//2
+    index_kanan = index_kiri + 1
+    median = (index_kiri + index_kanan)/2
+#Cetak hasil dengan format .1f (satu angka di belakang koma)
+print(f"{median:.1f}")
